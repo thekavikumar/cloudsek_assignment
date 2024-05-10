1f49e97d84bd11a9c968fd3363a96fe34f8ea75a
thekavikumar <kavikumarceo@gmail.com>
2024-05-10 21:51:48 +0530

    initial setup

---
+++ backend/index.ts
@@ -0,0 +1,15 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+const PORT: number = 3000;
+app.use(cors());
+
+// @ts-ignore
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
